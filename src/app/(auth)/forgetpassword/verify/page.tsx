9d6e9fbdbb914965401c4352227f6f079db39f54
HelmiDev <helmipaty@gmail.com>
2024-04-25 00:55:06 +0100

    done

--- src/app/(auth)/forgetpassword/verify/page.tsx
+++ src/app/(auth)/forgetpassword/verify/page.tsx
@@ -20,7 +20,7 @@ const Verify = () => {
     const tokenid = searchParams.get("token");
 
 
-    
+
     const expiredAt = searchParams.get("expiredAt");
     const timestampDate1 = new Date()
     const timestampDate2 = new Date(expiredAt ?? 0)
@@ -33,7 +33,7 @@ const Verify = () => {
     const errors = useSelector((state: any) => state.errors);
     const [otp, setOtp] = useState('');
     const [loading] = useState(false);
-    
+
 
 
 
@@ -42,38 +42,38 @@ const Verify = () => {
         if (!email) {
             router.push('/login');
         }
-        axios.post(process.env.NEXT_PUBLIC_DOMAIN+'/api/forgetpassword/verifytokenixist', { tokenid })
+        axios.post(process.env.NEXT_PUBLIC_DOMAIN + '/api/forgetpassword/verifytokenixist', { tokenid })
             .catch((err) => {
                 router.push('/login');
                 toast.error("time expired");
             });
-            const timer = setInterval(() => {
-                setTime((prevTime) => {
-                    if (prevTime === 0) {
-                        clearInterval(timer);
-                        router.push('/login');
-                        return 0;
-                    }
-                    return prevTime - 1;
-                });
-            }, 1000);
-    
-            return () => {
-                clearInterval(timer); // Clean up the timer when the component unmounts
-            };
-    },[]);
+        const timer = setInterval(() => {
+            setTime((prevTime) => {
+                if (prevTime === 0) {
+                    clearInterval(timer);
+                    router.push('/login');
+                    return 0;
+                }
+                return prevTime - 1;
+            });
+        }, 1000);
+
+        return () => {
+            clearInterval(timer); // Clean up the timer when the component unmounts
+        };
+    }, []);
     const minutes = Math.floor(time / 60).toString().padStart(2, '0');
     const seconds = (time % 60).toString().padStart(2, '0');
 
     const verify = async () => {
         dispatch({ type: 'ERRORS', payload: {} });
-        await axios.post(process.env.NEXT_PUBLIC_DOMAIN+'/api/forgetpassword/verifyotp', { email: email, token: otp })
+        await axios.post(process.env.NEXT_PUBLIC_DOMAIN + '/api/forgetpassword/verifyotp', { email: email, token: otp })
             .then((res) => {
                 router.push('/forgetpassword/verify/resetpassword?email=' + email + '&token=' + tokenid);
             })
-            .catch((err:any) => {
+            .catch((err: any) => {
                 console.log(err.response.data);
-               toast.error("Invalid OTP");
+                toast.error("Invalid OTP");
             });
     }
 
@@ -84,12 +84,13 @@ const Verify = () => {
             </div>
             <div className="w-[40%] flex flex-col justify-center items-center">
                 <div onClick={(e: any) => router.push('/login')}> <IoArrowBackCircleSharp className="absolute top-7 right-[37%] text-[35px] text-[#7152F3] hover:cursor-pointer" /></div>
-    
+
                 {/*Welcome && Please sign in here*/}
                 <div className="flex flex-col justify-center items-center gap-1">
-                    <p className={styles.title}>OTP Verification Code</p>
-                    <p className="text-xs">logged in successfully (an OTP has been sent to your email address)</p>
-                    <p className={styles.subtitle}>The OTP will expire in: {minutes}:{seconds} min</p>
+                    <p className={styles.title}>Reset Password</p>
+                    <div className='flex flex-row '>
+                        <p className="text-xs">an OTP has been sent to your email address : </p>
+                        <p style={{transform : 'translateY(-1px)' , marginLeft : "5px"}}  className={styles.subtitle}> expire in: {minutes}:{seconds} min</p></div>
                 </div>
                 <div className={`flex flex-col mt-8 mb-2 justify-center items-center h-[200px] w-[80%] bg-white-500 rounded-[10px] pt-[5px]`}>
                     <form className='mb-5 w-full flex flex-col justify-center items-center'>
@@ -104,19 +105,19 @@ const Verify = () => {
                                     <PinInputField />
                                 </PinInput>
                             </HStack>
-                          
+
                         </div>
                     </form>
                     <button onClick={verify} disabled={loading} className='mb-4 w-full flex justify-center items-center rounded-xl p-3 bg-[#115DFC] hover:bg-[#6790eb] disabled:bg-[#6790eb]' >
                         <span className='text-white font-semibold '>
-                            {loading ? ("Loading...") : ("Verify")}
+                            {loading ? ("Loading...") : ("Reset")}
                         </span>
                     </button>
                 </div>
             </div>
         </div>
-        
-     
+
+
     );
 }
 
